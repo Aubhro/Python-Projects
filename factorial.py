65af3a05c861dbcfa99e95f9038f412f1c1fb75f
Aubhro <aubrosengupta@gmail.com>
2016-01-01 16:12:56 -0800

    Factorial program
    A program that finds the factorial of a number with recursion and a
    while loop.

---
+++ factorial.py
@@ -0,0 +1,18 @@
+while True:
+	num = input("What number would you like to find the factorial of? \n")
+
+	def while_factorial(num):
+		factorial = 1
+		for i in range(num - 1):
+			factorial = factorial*(num - i)
+		return factorial
+	
+	def recur_factorial(num):
+		factorial = 1
+		if num > 1:
+			return num*recur_factorial(num - 1)
+		else:
+			return num
+	
+	print recur_factorial(num)
+	print while_factorial(num)		
