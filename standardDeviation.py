2e2ac43c4aa148db1fc3196a5f3856b479014524
Aubhro <aubrosengupta@gmail.com>
2016-02-21 16:51:06 -0800

    Fixed standard deviation
    added ability to input list

--- standardDeviation.py
+++ standardDeviation.py
@@ -1,12 +1,13 @@
 import math
-mainList = [0, 3, 5, 7, 2, 6, 1, 9, 12, 37, 42]
+# mainList = [0, 3, 5, 7, 2, 6, 1, 9, 12, 37, 42]
 
 
 def avg(data):
-    total = 0
+    total = 0.0
+    average = 0.0
     for i in range(len(data)):
         total = total + data[i]
-    average = total / len(data)
+    average = (total / (len(data)))
     return average
 
 
@@ -14,12 +15,21 @@ def deviation(data, average):
     difference = []
     for i in range(len(data)):
         difference.append((average - data[i])**2)
-    return math.sqrt(avg(data))
+    return math.sqrt(avg(difference))
 
 
 def find(data):
     average = avg(data)
     return deviation(data, average)
-
-
+mainList = []
+print("Enter list one number at a time, when you are finished, type done")
+while True:
+    item = raw_input("Item")
+    if item == "done":
+        break
+    try:
+        mainList.append(int(item))
+    except ValueError:
+        print ("Invalid item")
+print avg(mainList)
 print find(mainList)
